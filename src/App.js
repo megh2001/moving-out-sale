b914aa9ec6c60366a38974c0020135cb391b30e8
megh2001 <megh2001@gmail.com>
2025-08-04 20:04:04 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+import React from 'react';
 import './App.css';
 import ItemCard from './components/ItemCard';
 
@@ -6,21 +6,21 @@ const items = [
   {
     id: 1,
     title: "Ikea Sleeper Sofa",
-    description: "Ikea 2 seater sofa which becomes a very comfortable near queen size bed. Very good condition.\n\n\n https://www.ikea.com/us/en/p/lycksele-loevas-sleeper-sofa-ransta-natural-s79387027/",
+    description: "Ikea 2 seater sofa which becomes a very comfortable near queen size bed. Very good condition.\n\n https://www.ikea.com/us/en/p/lycksele-loevas-sleeper-sofa-ransta-natural-s79387027/",
     price: "$100 (Orignal price $450)",
     images: [
-      `${process.env.PUBLIC_URL}/images/sofa-bed-1.jpg`,
-      `${process.env.PUBLIC_URL}/images/sofa-bed-2.jpg`,
-      `${process.env.PUBLIC_URL}/images/sofa-bed-3.jpg`
+      "/images/sofa-bed-1.jpg",
+      "/images/sofa-bed-2.jpg",
+      "/images/sofa-bed-3.jpg"
     ]
   },
   {
     id: 2,
-    title: "Zinus Queen Memory Foam Mattress - 10 inches",
-    description: "11 months used, extremely comfortable. Selling for cheap because slightly stained.\n\n https://www.amazon.com/dp/B0CKYZ3B83?ref_=ppx_hzsearch_conn_dt_b_fed_asin_title_1&th=1",
+    title: "Zinus Queen Memory Foam Mattress - 12 inches",
+    description: "11 months used, extremely comfortable. Selling for cheap because slightly stained\n\n https://www.amazon.com/dp/B0CKYZ3B83?ref_=ppx_hzsearch_conn_dt_b_fed_asin_title_1&th=1",
     price: "$50 (Orignal price $260)",
     images: [
-      `${process.env.PUBLIC_URL}/images/mattress-1.jpg`
+      "/images/mattress-1.jpg"
     ]
   },
   {
@@ -29,17 +29,17 @@ const items = [
     description: "Very cool bar/storage. Only selling at a discount because the condition is not perfect",
     price: "$30",
     images: [
-      `${process.env.PUBLIC_URL}/images/bar-1.jpg`,
-      `${process.env.PUBLIC_URL}/images/bar-2.jpg`
+      "/images/bar-1.jpg",
+      "/images/bar-2.jpg"
     ]
   },
   {
     id: 4,
     title: "Storage Cabinet",
-    description: "Storage cabinet with 3 boxes and drawers.",
+    description: "Storage cabinet with 4 boxes and drawers.",
     price: "$20",
     images: [
-      `${process.env.PUBLIC_URL}/images/storage.jpg`
+      "/images/storage.jpg"
     ]
   }
 ];
